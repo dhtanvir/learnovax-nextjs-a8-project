c2270174e44dbd48d966c7dc3d46a2294ce8543e
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-04-30 18:52:35 +0600

    Navbar & Footer section add

--- src/components/shard/Navbar.jsx
+++ src/components/shard/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import NavLink from "./Navlink";
+import NavLink from "./NavLink";
 import Image from "next/image";
 import Link from "next/link";
-import logo from '@/assets/logo.png'
+import logo from "@/assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-gray-200 px-6  flex 
-    items-center justify-between">
+    <nav
+      className="w-full bg-gray-200 px-6  flex 
+    items-center justify-between"
+    >
       <div>
-        <Link  href={'/'}>
+        <Link href={"/"}>
           <Image src={logo} alt="Logo" width={190} height={50} />
         </Link>
       </div>
@@ -31,15 +33,7 @@ const Navbar = () => {
 
       <div className="flex items-center gap-4">
         <h2>Hello, </h2>
-        <div className="w-8 h-8 rounded-full overflow-hidden ">
-          {/* <Image
-                src="#"
-                alt="Profile"
-                width={48}
-                height={48}
-                className="w-full h-full object-cover"
-              /> */}
-        </div>
+        <div className="w-8 h-8 rounded-full overflow-hidden "></div>
 
         {/* Login Button */}
 
