c19b4b77bf4af4edc604d18b4388cd0ae5f7943b
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-05-01 18:37:52 +0600

    Top Instructors Section add

--- src/components/homePage/TopInstructorsSection.jsx
+++ src/components/homePage/TopInstructorsSection.jsx
@@ -108,7 +108,7 @@ const TopInstructorsSection = () => {
       
       {/* Title */}
       <h2 className="text-3xl font-bold text-center mb-10">
-        Top Instructors
+        Top Instructors   
       </h2>
 
       {/* Grid */}
