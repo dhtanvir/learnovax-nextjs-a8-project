6758d1ccf0825244ec78e84fb59d87db94fdff28
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-05-03 00:22:34 +0600

    Final Deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "https://learnovax-nextjs-a8-project-hpy4.vercel.app/"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://learnovax-nextjs-a8-project-hpy4.vercel.app/"
+    // baseURL: "http://localhost:3000"
 })
