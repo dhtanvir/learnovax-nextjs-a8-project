1a24e098fe1dbcd1f917be98b5afd66db6d03b5f
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-05-03 00:15:38 +0600

    proxy setUp

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -3,33 +3,37 @@
 import { UpdateUserModal } from "@/components/shard/UpdateUserModal";
 import { authClient } from "@/lib/auth-client";
 import { Avatar, Card } from "@heroui/react";
-
+import { BeatLoader } from "react-spinners";
 
 const ProfilePage = () => {
   const userData = authClient.useSession();
   const user = userData.data?.user;
 
-  if(!user) {
-    redirect('/signin')
+  if (userData.isPending) {
+    return (
+      <p className="text-center py-20">
+        <BeatLoader className="mx-auto" color="#3498db" />
+      </p>
+    );
   }
 
   return (
-    <div>
-        <h1>Profile</h1>
+    <div className="container mx-auto py-20">
+      <h1 className="text-center text-2xl font-bold">Profile</h1>
       <Card className="max-w-96 mx-auto flex flex-col items-center border mt-5">
         <Avatar className="h-20 w-20">
           <Avatar.Image
-            alt={user?.name}
+            alt={user?.name || "User"}
             src={user?.image}
             referrerPolicy="no-referrer"
           />
-          <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+          <Avatar.Fallback>{user?.name?.charAt(0) || "U"}</Avatar.Fallback>
         </Avatar>
 
         <h2 className="text-xl font-bold">{user?.name}</h2>
         <p className="text-muted">{user?.email}</p>
 
-        <UpdateUserModal/>
+        <UpdateUserModal />
       </Card>
     </div>
   );
